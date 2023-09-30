4a21f64492358f81afaecc7f39c88710ef7c85ab
Laz <lazgerasimov@gmail.com>
2023-09-30 18:12:37 +0100

    update job details section

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -3,7 +3,7 @@ import { Stack, useRouter, useGlobalSearchParams } from "expo-router";
 import { useCallback, useState } from "react";
 
 import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } from '../../components';
-import {COLORS, icons, SIZES} from '../../constants';
+import { COLORS, icons, SIZES } from '../../constants';
 import useFetch from "../../hook/useFetch";
 
 
@@ -12,14 +12,63 @@ const JobDetails = () => {
     const params = useGlobalSearchParams();
     const router = useRouter();
 
-    const {data, isLoading, error, refetch} = useFetch('job-details', {
+    const { data, isLoading, error, refetch } = useFetch('job-details', {
         job_id: params.id
-    })
+    });
+
+    cosnt[refreshing, setRefreshing] = useState(false);
+
+    const onRefresh = () => {
+
+    }
 
 
     return (
-        <SafeAreaView>
-            JobDetails
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerBackVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn
+                            iconUrl={icons.left}
+                            dimension="60%"
+                            handlePress={() => router.back()}
+                        />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn
+                            iconUrl={icons.share}
+                            dimension="60%"
+                        />
+                    ),
+                    headerTitle: ""
+                }}
+            />
+            <>
+                <ScrollView showsHorizontalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+                    {isLoading ? (
+                        <ActivityIndicator size={"large"} color={COLORS.primary} />
+                    ) : error ? (
+                        <Text>Something went wrong!</Text>
+                    ) : data.length === 0 ? (
+                        <Text>No Data</Text>
+                    ) : (
+                        <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+                            <Company
+                                companyLogo={data[0].employer_logo}
+                                jobTitle={data[0].job_title}
+                                companyName={data[0].employer_name}
+                                location={data[0].job_country}
+                            />
+                            <JobTabs
+
+                            />
+                        </View>
+                    )}
+                </ScrollView>
+            </>
         </SafeAreaView>
     )
 }
